c39da86133828c8adb6e61226486f8c09e28476d
GarLexS <alexander.garcia@est.uca.edu.ni>
2021-04-17 01:24:30 -0600

    Prueba

---
+++ ControlProteinasClient/ControlProteinasForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace ControlProteinasClient
+{
+    partial class ControlProteinasForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbUser = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAgregarUsuario = new System.Windows.Forms.Button();
+            this.txtMeta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblMeta = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtAgregar = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione un Usuario";
+            // 
+            // cmbUser
+            // 
+            this.cmbUser.FormattingEnabled = true;
+            this.cmbUser.Location = new System.Drawing.Point(12, 29);
+            this.cmbUser.Name = "cmbUser";
+            this.cmbUser.Size = new System.Drawing.Size(150, 24);
+            this.cmbUser.TabIndex = 1;
+            this.cmbUser.SelectedIndexChanged += new System.EventHandler(this.OnUserChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAgregarUsuario);
+            this.groupBox1.Controls.Add(this.txtMeta);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtUsuario);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 160);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agregar nuevo usuario:";
+            // 
+            // btnAgregarUsuario
+            // 
+            this.btnAgregarUsuario.Location = new System.Drawing.Point(9, 117);
+            this.btnAgregarUsuario.Name = "btnAgregarUsuario";
+            this.btnAgregarUsuario.Size = new System.Drawing.Size(75, 37);
+            this.btnAgregarUsuario.TabIndex = 4;
+            this.btnAgregarUsuario.Text = "Agregar";
+            this.btnAgregarUsuario.UseVisualStyleBackColor = true;
+            this.btnAgregarUsuario.Click += new System.EventHandler(this.btnAgregarUsuario_Click);
+            // 
+            // txtMeta
+            // 
+            this.txtMeta.Location = new System.Drawing.Point(74, 78);
+            this.txtMeta.Name = "txtMeta";
+            this.txtMeta.Size = new System.Drawing.Size(146, 22);
+            this.txtMeta.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Meta:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(74, 34);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(146, 22);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblMeta);
+            this.groupBox2.Controls.Add(this.lblTotal);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.txtAgregar);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(260, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 207);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Suma Proteina";
+            // 
+            // lblMeta
+            // 
+            this.lblMeta.AutoSize = true;
+            this.lblMeta.Location = new System.Drawing.Point(76, 164);
+            this.lblMeta.Name = "lblMeta";
+            this.lblMeta.Size = new System.Drawing.Size(16, 17);
+            this.lblMeta.TabIndex = 6;
+            this.lblMeta.Text = "0";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(76, 125);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(16, 17);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 17);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Meta:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Total:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Agregar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnAddProtein);
+            // 
+            // txtAgregar
+            // 
+            this.txtAgregar.Location = new System.Drawing.Point(76, 33);
+            this.txtAgregar.Name = "txtAgregar";
+            this.txtAgregar.Size = new System.Drawing.Size(118, 22);
+            this.txtAgregar.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Agregar:";
+            // 
+            // ControlProteinasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 271);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbUser);
+            this.Controls.Add(this.label1);
+            this.Name = "ControlProteinasForm";
+            this.Text = "ControlProteinasForm";
+            this.Load += new System.EventHandler(this.Onload);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbUser;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAgregarUsuario;
+        private System.Windows.Forms.TextBox txtMeta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblMeta;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtAgregar;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ControlProteinasClient/ControlProteinasForm.cs
@@ -0,0 +1,55 @@
+﻿using ControlProteinasClient.ServicioControlProteinas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlProteinasClient
+{
+    public partial class ControlProteinasForm : Form
+    {
+        private ProteinTrackingServiceSoapClient service =
+            new ProteinTrackingServiceSoapClient();
+
+        private User[] users;
+        public ControlProteinasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Onload(object sender, EventArgs e)
+        {
+            users = service.ListUsers();
+            cmbUser.DataSource = users;
+            cmbUser.DisplayMember = "Name";
+            cmbUser.ValueMember = "UserId";
+        }
+
+        private void OnUserChanged(object sender, EventArgs e)
+        {
+            var index = cmbUser.SelectedIndex;
+            lblTotal.Text = users[index].Total.ToString();
+            lblMeta.Text = users[index].Goal.ToString();
+        }
+
+        private void btnAgregarUsuario_Click(object sender, EventArgs e)
+        {
+            service.AddUser(txtUsuario.Text, int.Parse(txtMeta.Text));
+            users = service.ListUsers();
+            cmbUser.DataSource = users;
+        }
+
+        private void OnAddProtein(object sender, EventArgs e)
+        {
+            var userId = users[cmbUser.SelectedIndex].UserId;
+            var newTotal = service.AddProtein(int.Parse(txtAgregar.Text), userId);
+            users[cmbUser.SelectedIndex].Total = newTotal;
+            lblTotal.Text = newTotal.ToString();
+        }
+    }
+}
